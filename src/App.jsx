a26bf3883fad4f0b9750cc679a8731109970e392
mrBidone <ab.bz@ukr.net>
2024-11-29 19:43:04 +0100

    completed first theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Section from "./components/Section/Section";
+import ProfileList from "./components/ProfileList/ProfileList";
+import usersFromData from "./data/data.json";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <Section title="Contact List">
+        <ProfileList userList={usersFromData} />
+      </Section>
+      <Section title="">
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In, eligendi!
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <button>Lorem2</button>
+      </Section>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
