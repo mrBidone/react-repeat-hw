39cceb3b7819f5a7d4df0fb23b5869533c672cf3
mrBidone <ab.bz@ukr.net>
2024-11-30 10:23:54 +0100

    Add completed task1

---
+++ src/helpers/formatDateToNow.js
@@ -0,0 +1,7 @@
+import { formatDistanceToNow } from "date-fns";
+
+const formatDateToNow = (data) => {
+  return formatDistanceToNow(new Date(data), { addSuffix: true });
+};
+
+export default formatDateToNow;
